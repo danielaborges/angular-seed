75799e3f30955621964ca5255e3e881591ba04c9
Daniela Borges <alunassertiva@gmail.com>
2014-02-25 22:23:03 +0000

    new style, wish ordered by priority

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,18 +3,46 @@
 /* Controllers */
 
 var wishListControllers = angular.module('myApp.controllers', []);
+
 /*
-wishListControllers.controller('ShowListCtrl', ['$scope', 'Wish'
-   ,function($scope, Wish) {
-		$scope.wishes = Wish.query();		
+wishListControllers.controller('ShowListCtrl', ['$scope', 'Section', 'Wish'
+                                                ,function($scope, Section, Wish) {
+	 $scope.sections = Section.query();
+	 $scope.wishes = Wish.query();
+	 $scope.sectionName = 'pessoal';
 }]);*/
 
 wishListControllers.controller('ShowListCtrl', ['$scope', '$http'
      ,function($scope, $http) {
+	
+		//fazer funcao/servico loadData()
+		$http.get('json/sections.json').success(function(data){
+			$scope.sections = data;
+		});
 		$http.get('json/pessoal.json').success(function(data){
 			$scope.wishes = data;
 		});
+		
+		   
+		/*$scope.sections = loadData('json/sections.json');
+	    $scope.wishes = loadData('json/pessoal.json');
+		
+		var loadData = function(fileToLoad){
+			$http.get(fileToLoad).success(function(data){
+				return data;
+			});				
+		};*/
+		
 		$scope.sectionName = 'pessoal';
+		$scope.priority = 'priority'; 
+		
+		//fazer funcao/servico saveData()
+		$scope.save = function() {
+		    $http.post('json/pessoal.json', $scope.sections).then(function(data) {
+		      $scope.msg = 'Data saved';
+		    });
+		   // $scope.msg = 'Data sent: '+ JSON.stringify($scope.sections);
+		};
 }]);
 
 wishListControllers.controller('ShowSectionsCtrl', [function() {
--- app/js/services.js
+++ app/js/services.js
@@ -5,13 +5,6 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-var wishListServices = angular.module('myApp.services', ['ngResource']);
+var wishListServices = angular.module('myApp.services', []);
 
 wishListServices.value('version', '0.1');
-/*
-wishListServices.factory('Section', ['$resource',
-    function($resouce){
-		return $resource('json/:sectionId.json', {}, {
-			query: {method:'GET', params:{sectionId: 'sections'}, isArray:true}
-	    });
-}]);*/
