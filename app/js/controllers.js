164c536058362c5bdba75600b9780a0c0579adb1
Daniela Borges <alunassertiva@gmail.com>
2014-02-25 23:46:48 +0000

    wishlist.html divided by sections :)

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,53 +5,53 @@
 var wishListControllers = angular.module('myApp.controllers', []);
 
 /*
-wishListControllers.controller('ShowListCtrl', ['$scope', 'Section', 'Wish'
-                                                ,function($scope, Section, Wish) {
-	 $scope.sections = Section.query();
-	 $scope.wishes = Wish.query();
-	 $scope.sectionName = 'pessoal';
-}]);*/
+ wishListControllers.controller('ShowListCtrl', ['$scope', 'Section', 'Wish'
+ ,function($scope, Section, Wish) {
+ $scope.sections = Section.query();
+ $scope.wishes = Wish.query();
+ $scope.sectionName = 'pessoal';
+ }]);*/
 
-wishListControllers.controller('ShowListCtrl', ['$scope', '$http'
-     ,function($scope, $http) {
-	
-		//TODO:: funcao/servico loadData()
-		$http.get('json/sections.json').success(function(data){
-			$scope.sections = data;
-		});
-		$http.get('json/pessoal.json').success(function(data){
-			$scope.wishes = data;
-		});
-		
-		   
-		/*$scope.sections = loadData('json/sections.json');
-	    $scope.wishes = loadData('json/pessoal.json');
-		
-		var loadData = function(fileToLoad){
-			$http.get(fileToLoad).success(function(data){
-				return data;
-			});				
-		};*/
-		
-		$scope.sectionName = 'pessoal';
-		$scope.priority = 'priority'; 
-		
-		//TODO:: funcao/servico saveData()
-		$scope.save = function() {
-		    $http.post('json/pessoal.json', $scope.sections).then(function(data) {
-		      $scope.msg = 'Data saved';
-		    });
-		   // $scope.msg = 'Data sent: '+ JSON.stringify($scope.sections);
-		};
-}]);
+wishListControllers.controller('ShowListCtrl', [
+		'$scope',
+		'$http',
+		function($scope, $http) {
 
-wishListControllers.controller('ShowSectionsCtrl', [function() {
+			//TODO:: funcao/servico loadData()
+			$http.get('json/sections.json').success(function(data) {
+				$scope.sections = data;
+			});
+			$http.get('json/pessoal.json').success(function(data) {
+				$scope.wishes = data;
+			});
 
-}]);
+			/*$scope.sections = loadData('json/sections.json');
+			$scope.wishes = loadData('json/pessoal.json');
+			
+			var loadData = function(fileToLoad){
+				$http.get(fileToLoad).success(function(data){
+					return data;
+				});				
+			};*/
 
-wishListControllers.controller('ExpandSectionCtrl', ['$scope', '$routeParams',
-      function($scope, $routeParams) {
-	    $scope.sectionId = $routeParams.sectionId;
-}]);
+			$scope.sectionName = 'pessoal';
+			$scope.priority = 'priority';
 
+			//TODO:: funcao/servico saveData()
+			$scope.save = function() {
+				$http.post('json/pessoal.json', $scope.sections).then(
+						function(data) {
+							$scope.msg = 'Data saved';
+						});
+				// $scope.msg = 'Data sent: '+ JSON.stringify($scope.sections);
+			};
+		} ]);
 
+wishListControllers.controller('ShowSectionsCtrl', [ function() {
+
+} ]);
+
+wishListControllers.controller('ExpandSectionCtrl', [ '$scope', '$routeParams',
+		function($scope, $routeParams) {
+			$scope.sectionId = $routeParams.sectionId;
+		} ]);
